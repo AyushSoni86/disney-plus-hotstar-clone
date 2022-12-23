3b24fcc35aaf81fb64e16a2e960744a79e74d7af
Ayush Soni <ayusoni86@gmail.com>
2022-12-23 14:51:55 +0530

    Slider added

--- src/components/ImgSlider.js
+++ src/components/ImgSlider.js
@@ -8,7 +8,7 @@ function ImgSlider() {
   let settings = {
     dots: true,
     infinite: true,
-    speed: 500,
+    speed: 400,
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
@@ -19,7 +19,7 @@ function ImgSlider() {
         <img src="/images/slider-badging.jpg" alt="" />
       </Wrap>
       <Wrap>
-        <img src="/images/slider-badging.jpg" alt="" />
+        <img src="/images/slider-badag.jpg" alt="" />
       </Wrap>
     </Carousel>
   );
@@ -27,6 +27,40 @@ function ImgSlider() {
 
 export default ImgSlider;
 
-const Carousel = styled(Slider)``;
+const Carousel = styled(Slider)`
+  margin-top: 20px;
+  ul li button {
+    &:before {
+      font-size: 10px;
+      color: rgb(150, 158, 171);
+    }
+  }
 
-const Wrap = styled.div``;
+  li.slick-active button:before {
+    color: white;
+  }
+
+  .slick-list {
+    overflow: visible;
+  }
+
+  button{
+    z-index: 1;
+  }
+`;
+
+const Wrap = styled.div`
+cursor: pointer;
+  img {
+    border: 4px solid transparent;
+    width: 100%;
+    height: 100%;
+    border-radius: 5px;
+    box-shadow: rgb(0 0 0/69%) 0px 26px 30px -10px,
+      rgb(0 0 0/ 73%) 0px 16px 10px -10px;
+    transition-duration: 300ms;
+      &:hover{
+        border: 4px solid rgba(249,249,249,0.8);
+      }
+  }
+`;
